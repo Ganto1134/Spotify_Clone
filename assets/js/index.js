8e91509783c6f1a0982b5d7890ba7dc0009bd873
Antonio Gantolea <antoniogantolea@yahoo.it>
2024-05-14 12:43:14 +0200

    link altre pagine

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,9 @@ let cards = document.querySelectorAll('.carta');
 let titles = document.querySelectorAll('.titolo');
 let par = document.querySelectorAll('.par'); // Supponendo che questi siano i paragrafi
 let a = document.querySelectorAll('#overflow a')
+let artista = document.querySelectorAll('.carta .card-body a')
+let albumCentro = document.querySelectorAll('.albumino')
+let albumSx = document.querySelectorAll('.albumSx')
 
 let url = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=album%20pop';
 
@@ -26,6 +29,15 @@ fetch(url)
             if (img) {
                 img.src = canzone[i].album.cover_xl;
                 img.alt = `Cover di ${canzone[i].title}`;
+            }
+            if (artista[i]) {
+                artista[i].href = `artist.html?artistId=${canzone[i].artist.id}`;
+            }
+            if (albumCentro[i]) {
+                albumCentro[i].href = `album.html?albumtId=${canzone[i].album.id}`;
+            }
+            if (albumSx[i]) {
+                albumSx[i].href = `album.html?albumtId=${canzone[i].album.id}`;
             }
         }
     })
